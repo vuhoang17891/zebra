2c05cac0139a2f3deb2704d0831db2d3044f41c2
vuhoang17891 <vuhoang17891@gmail.com>
2018-11-19 03:06:28 -0800

    page model

--- zebra/models.py
+++ zebra/models.py
@@ -4,3 +4,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+    page_title= models.CharField(max_length=1000)
+    page_content= models.CharField(max_length=1000)

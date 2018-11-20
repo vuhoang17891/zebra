6539699002df76532490a70afde924c5c3435a4e
vuhoang17891 <vuhoang17891@gmail.com>
2018-11-19 18:48:30 -0800

    add more model

--- zebra/models.py
+++ zebra/models.py
@@ -6,4 +6,5 @@ from django.db import models
 # Create your models here.
 class Page(models.Model):
     page_title= models.CharField(max_length=1000)
+    page_title_2= models.CharField(max_length=100)
     page_content= models.CharField(max_length=1000)

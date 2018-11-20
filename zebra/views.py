09af60d7c1debabdbb36db564cd9a50490a8edee
vuhoang17891 <vuhoang17891@gmail.com>
2018-11-19 19:45:27 -0800

    fix views

--- zebra/views.py
+++ zebra/views.py
@@ -8,7 +8,9 @@ from .models import Page
 def who_we_are():
     return Page.objects.filter(title='who_we_are')
 
+print '+++++++++++++++++++++++' + who_we_are() + '+++++++++++++++++++++++++++'
+
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'home-corporate.html', context=None, who_we_are= who_we_are)
+        return render(request, 'home-corporate.html', context=None)

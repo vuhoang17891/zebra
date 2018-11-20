0ab8b3dc9a1d3a34fd02724592ec9a82a4fdd4a2
vuhoang17891 <vuhoang17891@gmail.com>
2018-11-19 19:40:49 -0800

    query model

--- zebra/views.py
+++ zebra/views.py
@@ -3,8 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Page
+
+def who_we_are():
+    return Page.objects.filter(title='who_we_are')
 
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'home-corporate.html', context=None)
+        return render(request, 'home-corporate.html', context=None, who_we_are= who_we_are())
